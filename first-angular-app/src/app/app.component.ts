2769b1e0c755678994e88af857ff0798b5e79b39
baobao@gmail.com <@Baobao999>
2024-06-27 01:04:27 +0700

    update project (show user + task + complete task)

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { NgFor, NgIf } from '@angular/common';
 
+import { HeaderComponet } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponet, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  users = DUMMY_USERS;
+  private selectedUserId?: string;
+
+  get selectUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onSelectUser = (id: string) => {
+    this.selectedUserId = id;
+  };
 }
