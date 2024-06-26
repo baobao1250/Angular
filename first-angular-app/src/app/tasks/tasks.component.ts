59a140f81761a8404f7f58f86c75b1139b1b73a8
baobao@gmail.com <@Baobao999>
2024-06-27 01:20:24 +0700

    add ui create task (project 1)

--- first-angular-app/src/app/tasks/tasks.component.ts
+++ first-angular-app/src/app/tasks/tasks.component.ts
@@ -1,21 +1,26 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { DUMMY_TASKS } from './dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+  isAddingTask: boolean = false;
   private tasks = DUMMY_TASKS;
 
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId);
+  }
+  OnStartAddTask() {
+    this.isAddingTask = true;
   }
 
   OnCompleteTask(taskId: string) {
