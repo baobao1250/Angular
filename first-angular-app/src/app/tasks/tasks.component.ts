bcbd7dc366c29620ac0aa58d5daf62f3968a598e
baobao@gmail.com <@Baobao999>
2024-06-28 19:38:52 +0700

    finish first app

--- first-angular-app/src/app/tasks/tasks.component.ts
+++ first-angular-app/src/app/tasks/tasks.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { DUMMY_TASKS } from './dummy-tasks';
 import { NewTaskComponent } from './new-task/new-task.component';
-
+import { type Task } from './task/task.model';
+import { TasksService } from './tasks.service';
 @Component({
   selector: 'app-tasks',
   standalone: true,
@@ -14,16 +15,26 @@ export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
   isAddingTask: boolean = false;
-  private tasks = DUMMY_TASKS;
+
+  // private taskService = new TaskService(); // khi gọi sẽ tạo ra 1 đối
+  // tượng ở đây và ko chia sẽ ra ngoài.
+  // Nên mỗi lần gọi sẽ tạo ta 1 đối tượng mới
+
+  constructor(private tasksService: TasksService) {}
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId);
-  }
-  OnStartAddTask() {
-    this.isAddingTask = true;
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   OnCompleteTask(taskId: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    return this.tasksService.removeTask(taskId);
   }
+
+  OnOpenDialog(isClosed: boolean) {
+    this.isAddingTask = isClosed;
+  }
+  // OnAddTask(task: Task) {
+  //   this.isAddingTask = false;
+  //   return this.taskService.addTask(task);
+  // }
 }
