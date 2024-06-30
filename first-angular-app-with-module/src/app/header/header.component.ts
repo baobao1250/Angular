d6a18a685f4a91ec71f32372d39bcb379c62b1e3
baobao@gmail.com <@Baobao999>
2024-07-01 02:26:47 +0700

    update

--- first-angular-app-with-module/src/app/header/header.component.ts
+++ first-angular-app-with-module/src/app/header/header.component.ts
@@ -6,4 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponet {}
+export class HeaderComponent {}
